2dd67cfeeb298f15e01cdc508329fff1405fef24
G Narmada <93468483+narmadagogineni@users.noreply.github.com>
2023-09-06 15:04:32 +0530

    Add files via upload

---
+++ calci.py
@@ -0,0 +1,10 @@
+print(".........***CALCULATOR USING PYTHON***..........")
+
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+
+print("ADDITION", a+b)
+print("SUBSTRACTION", a-b)
+print("MULTIPLICATION", a*b)
+print("DIVISION", a//b)
+print("MODULO", a%b)
